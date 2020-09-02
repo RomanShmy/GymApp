c51c9dd0e5e7b3c2cefa64476cb1053b829e2948
Shmyhol Roman <shmigol.roma@gmail.com>
2020-09-02 14:22:52 +0300

    Add migration

---
+++ Migrations/Migration_20200902.cs
@@ -0,0 +1,28 @@
+using FluentMigrator;
+
+namespace GymApp.Migrations
+{
+    [Migration(20200902, "IntialMigrate")]
+    public class Migration_20200902 : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("Transactions");
+            Delete.Table("Account");
+        }
+
+        public override void Up()
+        {
+            Create.Table("Account")
+                .WithColumn("Id").AsInt64().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Amount").AsDecimal();
+                
+            Create.Table("Transactions")
+                .WithColumn("Id").AsInt64().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Amount").AsDecimal()
+                .WithColumn("Descriptions").AsString().NotNullable()
+                .WithColumn("Date").AsDateTime()
+                .WithColumn("AccountId").AsInt64().ForeignKey("Account", "Id");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,10 @@
-using System.Transactions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using FluentMigrator.Runner;
+using GymApp.Migrations;
 
 namespace GymApp
 {
@@ -27,10 +21,20 @@ namespace GymApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
             var connectionString = new ConnectionString(Configuration.GetConnectionString("DefaultConnection"));
             services.AddSingleton(connectionString);
+
             services.AddScoped<ITransactionRepository, TransactionRepository>();
             services.AddScoped<IAccountRepository, AccountRepository>();
+
+            services.AddFluentMigratorCore()
+                    .ConfigureRunner( builder => 
+                        builder.AddPostgres()
+                        .WithGlobalConnectionString(connectionString.Value)
+                        .ScanIn(typeof(object).Assembly).For.Migrations()
+
+                    );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +55,8 @@ namespace GymApp
             {
                 endpoints.MapControllers();
             });
+
+            app.Migrate();
         }
     }
 }
